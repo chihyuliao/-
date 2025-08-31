269e03fb20670bc8aad560ed52c3ec0d2ae84732
chihyuliao <chihyu.htc@gmail.com>
2025-08-31 10:11:09 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,14 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import "./globals.css";
 
 export const metadata = {
-  title: '我讀字升級',
-  description: '互動式英文學習平台',
+  title: "我讀字升級",
+ 
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="zh-Hant">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
