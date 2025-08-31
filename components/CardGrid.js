ffea77bb7b9d6be89f4e1ba1bc6ed45f9d07115e
chihyuliao <chihyu.htc@gmail.com>
2025-08-31 10:14:55 +0800

    Create CardGrid.js

---
+++ components/CardGrid.js
@@ -0,0 +1,22 @@
+mport React from "react";
+
+export default function CardGrid({ active }) {
+  const cards = [
+    `${active} Card 1`,
+    `${active} Card 2`,
+    `${active} Card 3`,
+    `${active} Card 4`,
+    `${active} Card 5`,
+    `${active} Card 6`,
+  ];
+
+  return (
+    <div className="card-grid">
+      {cards.map((c, i) => (
+        <div key={i} className="card">
+          {c}
+        </div>
+      ))}
+    </div>
+  );
+}
