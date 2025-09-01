77c581fc368b210ec8c2b20a127475967a55c823
chihyuliao <chihyu.htc@gmail.com>
2025-09-01 08:59:32 +0800

    Update page.js

--- app/listening/page.js
+++ app/listening/page.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import { useState } from "react";
 
-export default function ListeningPage() {
+export default function listeningPage() {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
